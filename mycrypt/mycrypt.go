2e359c04983e681c9b118da6ae94c9277851f74b
Masa-44 <masa_40@outlook.com>
2023-04-22 21:54:17 +0000

    okey

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,28 +1,28 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789ABCDEFGHIJKLMONPQRSTUWXYZÆØÅ.,:; ")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyz      0123456789ABCDEFGHIJKLMONPQRSTUWXYZ ^f ^x ^e.,:; ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
-	kryptertMelding := make([]rune, len(melding))
-	for i := 0; i < len(melding); i++ {
-		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
-			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
-		} else {
-			kryptertMelding[i] = alphabet[indeks+chiffer]
-		}
-	}
-	return kryptertMelding
+        kryptertMelding := make([]rune, len(melding))
+        for i := 0; i < len(melding); i++ {
+                indeks := sokIAlfabetet(melding[i], alphabet)
+                if indeks+chiffer >= len(alphabet) {
+                        kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+                } else {
+                        kryptertMelding[i] = alphabet[indeks+chiffer]
+                }
+        }
+        return kryptertMelding
 }
 
 func sokIAlfabetet(symbol rune, alfabet []rune) int {
-	for i := 0; i < len(alfabet); i++ {
-		if symbol == alfabet[i] {
-			return i
-			break
-		}
-	}
-	return -1
+        for i := 0; i < len(alfabet); i++ {
+                if symbol == alfabet[i] {
+                        return i
+                        break
+                }
+        }
+        return -1
 }
 func Dekrypter(melding []rune, alphabet []rune, chiffer int) []rune {
         dekryptertMelding := make([]rune, len(melding))
